72f36e143aeffb1487deb1d2ba9d28768fe68877
ignitedevua <ignitedevua@gmail.com>
2022-03-05 21:00:48 +0200

    init

---
+++ index.js
@@ -0,0 +1,40 @@
+fetch('stats.json')
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        layoutData(data);
+    })
+    .catch(function(err) {
+        console.log('error: ' + err);
+    });
+
+function layoutData(data) {
+    var date_div = document.getElementById("date");
+    var money_div = document.getElementById("total_money");
+    date_div.innerHTML = data.date;
+
+    var total_money = 0;
+
+    var mainContainer = document.getElementById("stats");
+    console.log(data);
+    for (var i = 0; i < data.list.length; i++) {
+
+        var div = document.createElement("div");
+        var price = data.list[i].p * data.list[i].c;
+        // div.className += ' item';
+        div.innerHTML = `
+        <div class=" item">\
+                <div class="icon inl ${data.list[i].i}"></div>\
+                <div class="name inl">${data.list[i].t}</div>\
+                <div class="count inl">${data.list[i].c}</div>\
+                <div class="price inl">${price} млн.</div>\
+            </div>`;
+        div = div.children[0];
+        mainContainer.appendChild(div);
+
+        total_money += price;
+    }
+
+    money_div.innerText = (total_money / 1000).toFixed(2) + " млрд";
+}
